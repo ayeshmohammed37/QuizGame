38cb7eade908c0239c44f4e6ed16bf16d0055459
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2025-07-16 20:47:05 +0300

    Initial Creation of Quiz Game

---
+++ QuizGame/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace QuizGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] questions = new string[20]
+            {
+                "What is the capital of France?",
+                "What is 5 + 7?",
+                "Who wrote 'Romeo and Juliet'?",
+                "What is the largest planet in our solar system?",
+                "Which element has the chemical symbol 'O'?",
+                "What is the fastest land animal?",
+                "Who painted the Mona Lisa?",
+                "What is the smallest prime number?",
+                "What is the boiling point of water in Celsius?",
+                "Which country is known as the Land of the Rising Sun?",
+                "What is the square root of 64?",
+                "What is the main language spoken in Brazil?",
+                "Which ocean is the deepest?",
+                "Who discovered gravity?",
+                "What color do you get when you mix red and white?",
+                "What year did World War II end?",
+                "What is the tallest mountain in the world?",
+                "What is the chemical formula of water?",
+                "Which planet is known as the Red Planet?",
+                "Who is known as the father of computers?"
+            };
+
+            string[] answers = new string[20]
+            {
+                "Paris",
+                "12",
+                "William Shakespeare",
+                "Jupiter",
+                "Oxygen",
+                "Cheetah",
+                "Leonardo da Vinci",
+                "2",
+                "100",
+                "Japan",
+                "8",
+                "Portuguese",
+                "Pacific Ocean",
+                "Isaac Newton",
+                "Pink",
+                "1945",
+                "Mount Everest",
+                "H2O",
+                "Mars",
+                "Charles Babbage"
+            };
+        }
+    }
+}
